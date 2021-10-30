4846c08eacfaf9206ffd0f39c81269ed029f6c29
manisankarsms <manisankarsms@gmail.com>
2021-10-30 12:07:02 +0530

    emi formula updated

--- app/src/main/java/com/techbros/emicalculator/Home.java
+++ app/src/main/java/com/techbros/emicalculator/Home.java
@@ -44,11 +44,13 @@ public class Home extends AppCompatActivity {
                 double p =Double.parseDouble(filler);
                 double r = Double.parseDouble(filler2);
                 //EMI = p × r × (1 + r)n/((1 + r)n - 1)
-                r = r / 100; // one month interest
+                r = r / 1200; // one month interest
 //                t = t * 12; // one month period
 //                emi = (p * r * (float)Math.pow(1 + r, t))
 //                        / (float)(Math.pow(1 + r, t) - 1);
-                listItems.add("3 Months "+(p * r * (float)Math.pow(1 + r, 3)) / (float)(Math.pow(1 + r, 3) - 1));
+                double total = (p * r * (float)Math.pow(1 + r, 3)) / (float)(Math.pow(1 + r, 3) - 1)*3;
+                listItems.add("3 Installments"+Math.round(p * r * (float)Math.pow(1 + r, 3)/ (float)(Math.pow(1 + r, 3) - 1))+"\tTotal "+Math.round(total));
+                listItems.add("6 Installments"+Math.round(p * r * (float)Math.pow(1 + r, 6)/ (float)(Math.pow(1 + r, 6) - 1))+"\tTotal "+Math.round((p * r * (float)Math.pow(1 + r, 6)/ (float)(Math.pow(1 + r, 6) - 1))*6));
 //                listItems.add("6 Months"+((numamount*numpercent*(1+numpercent)*6)/((1+numpercent)*6-1)));
 //                listItems.add("9 Months"+((numamount*numpercent*(1+numpercent)*9)/((1+numpercent)*9-1)));
 //                listItems.add("12 Months"+((numamount*numpercent*(1+numpercent)*12)/((1+numpercent)*12-1)));
